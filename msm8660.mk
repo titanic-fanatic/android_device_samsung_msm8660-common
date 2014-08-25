8d4a5d7914de1e1ba24ce465dd0937b1a0851d14
Tylor Janzen <tylor.janzen@gmail.com>
2014-08-24 18:24:50 -0600

    audio.r_submix.default: Build audio.r_submix.default to enable mirroring.
        * Added configuration for submix and input
        * Added "config_enableWifiDisplay" to overlay

--- msm8660.mk
+++ msm8660.mk
@@ -75,11 +75,15 @@ PRODUCT_PACKAGES += \
     lpm.rc \
     ueventd.qcom.rc
 
+# Audio config
+PRODUCT_COPY_FILES += \
+    device/samsung/msm8660-common/configs/audio_policy.conf:system/etc/audio_policy.conf
+
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
+    audio.r_submix.default \
     audio.usb.default \
-    audio_policy.conf \
     audio_policy.msm8660 \
     audio.primary.msm8660 \
     libaudio-resampler \
