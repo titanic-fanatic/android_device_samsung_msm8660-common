322522551fc2238c19de27182dfaa7b3f499a81d
Tylor Janzen <tylor.janzen@gmail.com>
2015-02-24 17:14:30 -0700

    GPS: Copy gps.conf to /system/etc and add canadian NTP server

--- msm8660.mk
+++ msm8660.mk
@@ -117,6 +117,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     gps.msm8660
 
+# GPS configuration file for msm8660
+PRODUCT_COPY_FILES += \
+	device/samsung/msm8660-common/gps/gps.conf:/system/etc/gps.conf
+
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/8660_handset.kl:system/usr/keylayout/8660_handset.kl\
